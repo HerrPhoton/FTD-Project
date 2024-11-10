6ffa772e41067378d6247fa57c6fdc3192210ef3
Aleksandr Leisle <113707601+HerrPhoton@users.noreply.github.com>
2024-11-11 01:24:54 +0700

    Remove react-shadow

--- frontend/src/components/APIPanel.js
+++ frontend/src/components/APIPanel.js
@@ -1,18 +1,16 @@
 import SwaggerUI from 'swagger-ui-react';
-import root from 'react-shadow';
-import { API_URL } from '../config' 
+import { API_URL } from '../config'
 function APIPanel() {
     return (
         <div className='intro'>
-                    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-react/swagger-ui.css" />
-                    <SwaggerUI
-                        url={`${API_URL}/openapi.json`} 
-                        docExpansion="list"
-                        supportedSubmitMethods={['get', 'post', 'put', 'delete']}
-                    />
+            <link rel="stylesheet" href="https://unpkg.com/swagger-ui-react/swagger-ui.css" />
+            <SwaggerUI
+                url={`${API_URL}/openapi.json`}
+                docExpansion="list"
+                supportedSubmitMethods={['get', 'post', 'put', 'delete']}
+            />
 
         </div>
     );
 };
 export default APIPanel;
-
